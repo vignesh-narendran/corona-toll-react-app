d66a9e6ebe6d394186dee6a33ec113f46ea22237
Vignesh Narendran <vignesh-narendran@users.noreply.github.com>
2020-03-21 11:29:19 +0530

    Updates

---
+++ precache-manifest.b75d79adcb2f5cb5727b53275be1c7ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d72f77a510957afe276d6581245fe3",
+    "url": "/corona-toll-react-app/index.html"
+  },
+  {
+    "revision": "8f173b16b066c095dc85",
+    "url": "/corona-toll-react-app/static/css/main.d82cfb86.chunk.css"
+  },
+  {
+    "revision": "bb319f2a9484a6d0087d",
+    "url": "/corona-toll-react-app/static/js/2.55104385.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/corona-toll-react-app/static/js/2.55104385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f173b16b066c095dc85",
+    "url": "/corona-toll-react-app/static/js/main.318950dd.chunk.js"
+  },
+  {
+    "revision": "d6f1259004db96834415",
+    "url": "/corona-toll-react-app/static/js/runtime-main.dafd2c55.js"
+  },
+  {
+    "revision": "93e2c2db0d94715b21fe535c14e26f13",
+    "url": "/corona-toll-react-app/static/media/loader.93e2c2db.gif"
+  }
+]);
